b55a47ebd44dd8b78dbe00a6d04f6191b9414f48
Ahmed T. Ali <ah.tajelsir@gmail.com>
2019-03-06 17:50:15 +0300

    feat: support all JS extensions

--- src/matchers/node.js
+++ src/matchers/node.js
@@ -9,7 +9,7 @@ const match = require('micromatch');
  */
 module.exports = async function(files) {
 	// The pattern we test against
-	const pattern = '**/package.json';
+	const pattern = ['**/package.json', '**/*.{js,jsx,mjs,ts,tsx}'];
 
 	// Try to find 'package.json' ?
 	return match(files, pattern).length > 0;
--- src/matchers/tests/node.test.js
+++ src/matchers/tests/node.test.js
@@ -4,11 +4,30 @@
 const matcher = require('../node');
 
 describe('node', () => {
-	it('matches only when a package.json exists', async () => {
+	it('matches when a package.json exists', async () => {
 		const files = ['package.json'];
 		expect(await matcher(files)).toEqual(true);
 
 		const fake = ['.pachage.json', 'fakepackage.json', 'pachage.jso'];
 		expect(await matcher(fake)).toEqual(false);
+	});
+
+	it('matches when a JavaScript file exists', async () => {
+		let files = ['package.js'];
+		expect(await matcher(files)).toEqual(true);
+
+		files = ['something.mjs'];
+		expect(await matcher(files)).toEqual(true);
+
+		files = ['src/something.jsx'];
+		expect(await matcher(files)).toEqual(true);
+	});
+
+	it('matches when a TypeScript file exists', async () => {
+		let files = ['src/package.ts'];
+		expect(await matcher(files)).toEqual(true);
+
+		files = ['src/package.tsx'];
+		expect(await matcher(files)).toEqual(true);
 	});
 });
